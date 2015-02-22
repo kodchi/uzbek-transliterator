649e9c800a3d8c60578dad9a0d31dc08cee338cb
Bahodir Mansurov <bmansurov@wikimedia.org>
2015-02-21 19:37:52 -0500

    Add to_latin

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #uzbek-transliterator
 
 Transliterate Cyrillic Uzbek text to Latin and vice versa.
+https://uz.wikipedia.org/wiki/Vikipediya:O%CA%BBzbek_lotin_alifbosi_qoidalari
---
+++ transliterate.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+import re
+
+
+LATIN_TO_CYRILLIC = {
+    'a': 'а', 'A': 'А',
+    'b': 'б', 'B': 'Б',
+    'd': 'д', 'D': 'Д',
+    'e': 'е', 'E': 'Е',
+    'f': 'ф', 'F': 'Ф',
+    'g': 'г', 'G': 'Г',
+    'h': 'ҳ', 'H': 'Ҳ',
+    'i': 'и', 'I': 'И',
+    'j': 'ж', 'J': 'Ж',
+    'k': 'к', 'K': 'К',
+    'l': 'л', 'L': 'Л',
+    'm': 'м', 'M': 'М',
+    'n': 'н', 'N': 'Н',
+    'o': 'о', 'O': 'О',
+    'p': 'п', 'P': 'П',
+    'q': 'қ', 'Q': 'Қ',
+    'r': 'р', 'R': 'Р',
+    's': 'с', 'S': 'С',
+    't': 'т', 'T': 'Т',
+    'u': 'у', 'U': 'У',
+    'v': 'в', 'V': 'В',
+    'x': 'х', 'X': 'Х',
+    'y': 'й', 'Y': 'Й',
+    'z': 'з', 'Z': 'З',
+    'o‘': 'ў', 'O‘': 'Ў',
+    'oʻ': 'ў', 'Oʻ': 'Ў',
+    'g‘': 'ғ', 'G‘': 'Ғ',
+    'gʻ': 'ғ', 'Gʻ': 'Ғ',
+    'sh': 'ш', 'Sh': 'Ш', 'SH': 'Ш',
+    'ch': 'ч', 'Ch': 'Ч', 'CH': 'Ч',
+    'yo‘': 'йў', 'Yo‘': 'Йў', 'YO‘': 'ЙЎ',
+    'yoʻ': 'йў', 'Yoʻ': 'Йў', 'YOʻ': 'ЙЎ',
+    'ts': 'ц', 'Ts': 'Ц', 'TS': 'Ц',
+    'yo': 'ё', 'Yo': 'Ё', 'YO': 'Ё',
+    'yu': 'ю', 'Yu': 'Ю', 'YU': 'Ю',
+    'ya': 'я', 'Ya': 'Я', 'YA': 'Я',
+    'ye': 'е', 'Ye': 'Е', 'YE': 'Е',
+    'ʼ': 'ъ',  # TODO: case?
+}
+LATIN_CONSONANTS = (
+    'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'o‘', 'O‘'
+)
+
+CYRILLIC_TO_LATIN = {
+    'а': 'a', 'А': 'A',
+    'б': 'b', 'Б': 'B',
+    'в': 'v', 'В': 'V',
+    'г': 'g', 'Г': 'G',
+    'д': 'd', 'Д': 'D',
+    'е': 'e', 'Е': 'E',
+    'ё': 'yo', 'Ё': 'Yo',
+    'ж': 'j', 'Ж': 'J',
+    'з': 'z', 'З': 'Z',
+    'и': 'i', 'И': 'I',
+    'й': 'y', 'Й': 'Y',
+    'к': 'k', 'К': 'K',
+    'л': 'l', 'Л': 'L',
+    'м': 'm', 'М': 'M',
+    'н': 'n', 'Н': 'N',
+    'о': 'o', 'О': 'O',
+    'п': 'p', 'П': 'P',
+    'р': 'r', 'Р': 'R',
+    'с': 's', 'С': 'S',
+    'т': 't', 'Т': 'T',
+    'у': 'u', 'У': 'U',
+    'ф': 'f', 'Ф': 'F',
+    'х': 'x', 'Х': 'X',
+    'ц': 's', 'Ц': 'S',
+    'ч': 'ch', 'Ч': 'Ch',
+    'ш': 'sh', 'Ш': 'Sh',
+    'ъ': 'ʼ', 'Ъ': 'ʼ',
+    'ь': '', 'Ь': '',
+    'э': 'e', 'Э': 'E',
+    'ю': 'yu', 'Ю': 'Yu',
+    'я': 'ya', 'Я': 'Ya',
+    'ў': 'oʻ', 'Ў': 'Oʻ',
+    'қ': 'q', 'Қ': 'Q',
+    'ғ': 'gʻ', 'Ғ': 'Gʻ',
+    'ҳ': 'h', 'Ҳ': 'H',
+}
+CYRILLIC_CONSONANTS = (
+    'а', 'А', 'е', 'Е', 'ё', 'Ё', 'и', 'И', 'о', 'О', 'у', 'У', 'э', 'Э', 'ю', 'Ю', 'я', 'Я', 'ў', 'Ў'
+)
+
+
+def to_cyrillic(text):
+    """Transliterate latin text to cyrillic  using the following rules:
+    """
+    return text
+
+
+def to_latin(text):
+    """Transliterate cyrillic text to latin using the following rules:
+    1. ц = s at the beginning of a word or in the middle of a word after consonant sound.
+    ц = ts in the middle of a word after a vowel sound.
+        цирк = sirk
+        цех = sex
+        федерация = federatsiya
+        функция = funksiya
+    """
+    # rule 1
+    # regex = re.compile('\b([цЦ])')
+    # text = regex.sub(lambda x: 's' if x == 'ц' else 'S', text)
+
+    return text
+
+
+def transliterate(text, to_variant):
+    if to_variant == 'cyrillic':
+        text = to_cyrillic(text)
+    elif to_variant == 'latin':
+        text = to_latin(text)
+
+    return text
